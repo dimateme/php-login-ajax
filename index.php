018e18734a6360028fd66d7071af06cb394c6741
dimateme <jipidimatem@gmail.com>
2023-07-07 11:12:13 -0400

    connection à la base de donnée

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 </div>
 
 <!-- jQuery is required -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 
 <!-- UIkit JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
